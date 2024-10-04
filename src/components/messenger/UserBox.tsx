473ce77c8aa325afadbf6c84b61bdbf6201fc600
jquizRaja <rajajobhikar@gmail.com>
2024-10-04 21:39:15 +0530

    finish people page

--- src/components/messenger/UserBox.tsx
+++ src/components/messenger/UserBox.tsx
@@ -1,29 +1,65 @@
-"use client"
-import React, { useCallback, useState } from 'react'
-import { User } from '@prisma/client'
-import { useRouter } from 'next/navigation';
-import axios from 'axios';
+"use client";
+import React, { useCallback, useState } from "react";
+import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import AvatarImg from "../main/Avatar";
 
-interface UserBoxProps{
-  data:User
+interface UserBoxProps {
+  data: User;
 }
 
-const UserBox:React.FC<UserBoxProps> = ({
-  data,
-}) => {
-  const router= useRouter()
-  const [isLoading,SetIsLoading]=useState(false)
-  const handleClick=useCallback(()=>{
-    SetIsLoading(true)
-    axios.post("/api/messenger/conversations",{
-      userId:data.id
-    })
-  },[])
+const UserBox: React.FC<UserBoxProps> = ({ data }) => {
+  const router = useRouter();
+  const [isLoading, SetIsLoading] = useState(false);
+  const handleClick = useCallback(() => {
+    SetIsLoading(true);
+    //here private chat is insiatated with clicking on it
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => {
+        SetIsLoading(false);
+      });
+  }, [data, router]);
   return (
-    <div>
-
+    <div
+      onClick={handleClick}
+      className="
+    w-full
+    relative
+    flex
+    items-center
+    space-x-3
+    bg-white
+    p-3
+    hover:bg-neutral-200
+    rounded-lg
+    transition
+    cursor pointer
+    "
+    >
+      <AvatarImg user={data} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div
+            className="
+          flex 
+          justify-between
+          items-center
+          mb-1
+          "
+          >
+            <p className="text-sm font-medium">{data.name}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserBox
+export default UserBox;
